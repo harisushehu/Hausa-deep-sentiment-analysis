028f3648dab8dbf3108f311d9b103b9d971d10c3
harisushehu <34377892+harisushehu@users.noreply.github.com>
2023-12-27 13:19:16 +1300

    Update ANOVA.py

--- ANOVA.py
+++ ANOVA.py
@@ -7,9 +7,68 @@ Created on Sat Dec 23 10:01:35 2023
 """
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import f_oneway
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
+
+# Read data from CSV files
+cnn_data = pd.read_csv('cnn_performance.csv')
+rnn_data = pd.read_csv('rnn_performance.csv')
+hnn_data = pd.read_csv('hnn_performance.csv')
+
+# Extract accuracy columns from each DataFrame
+cnn_accuracy = cnn_data['accuracy']
+rnn_accuracy = rnn_data['accuracy']
+hnn_accuracy = hnn_data['accuracy']
+
+# Combine data into a single DataFrame
+combined_data = pd.concat([cnn_accuracy, rnn_accuracy, hnn_accuracy], axis=1, keys=['CNN', 'RNN', 'HNN'])
+
+# Set up the boxplot
+plt.figure(figsize=(15, 10))
+sns.set_theme(style="whitegrid")
+metrics = ['accuracy']
+
+for i, metric in enumerate(metrics, 1):
+    plt.subplot(2, 2, i)
+    sns.boxplot(data=combined_data)
+    plt.title(f'{metric.capitalize()} Comparison')
+    plt.ylabel(metric.capitalize())
+
+# ANOVA analysis
+f_statistic, p_value_anova = f_oneway(cnn_accuracy, rnn_accuracy, hnn_accuracy)
+print(f'ANOVA results: F-statistic={f_statistic}, p-value={p_value_anova}')
+
+# Combine data into a single DataFrame
+flattened_data = pd.concat([cnn_accuracy, rnn_accuracy, hnn_accuracy]).values.flatten()
+group_labels = ['CNN'] * len(cnn_accuracy) + ['RNN'] * len(rnn_accuracy) + ['HNN'] * len(hnn_accuracy)
+
+# Perform Tukey's test
+tukey_results = pairwise_tukeyhsd(flattened_data, group_labels)
+
+
+# Plot significance results in a table
+tukey_summary = tukey_results.summary()
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.axis('off')  # Turn off the axis
+
+# Create a table from the tukey summary data
+table_data = []
+for row in tukey_summary.data:
+    table_data.append([row[0], row[1], row[2], row[3]])
+
+table_headers = ['Group 1', 'Group 2', 'Mean Diff', 'P-Value']
+ax.table(cellText=table_data, colLabels=table_headers, loc='center')
+
+plt.savefig("performance_with_significance.png")
+plt.show()
+
+
+#************************************ ANOVA detailed result ************************************************#
 
 # Read data from CSV files
 cnn_data = pd.read_csv('cnn_performance.csv')
@@ -48,9 +107,4 @@ def anova_table(aov):
 # Calculate ANOVA table with mean squared, eta squared, and omega squared
 anova_table = anova_table(aov_table)
 print(anova_table)
-
-
-
-
-
 
